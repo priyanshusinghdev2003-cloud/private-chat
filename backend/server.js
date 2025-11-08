a5975474d756deac800d7bfc4c3d47cc2e5acb9a
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2025-11-08 16:51:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -91,11 +91,11 @@ io.on("connection", (socket) => {
 
 // ---- Serve Vite Build ---- //
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))
-  app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
-    
-  })
+  app.use(express.static(path.join(__dirname, "public"))); // from backend/public
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 }
 
 
